eb9af31370db94589e2314caf0f16f85c83a33e7
Bun <bunthang204@gmail.com>
2024-07-10 16:12:44 +0700

    DB Conntection

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
+  const { MONGO_URI } = process.env;
+
+  if (!MONGO_URI) {
+    throw new Error("Missing MONGO_URI environment variable.");
+  }
+
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(MONGO_URI, {
       dbName: "HOSPITAL_MANAGEMENT_SYSTEM",
     })
     .then(() => {
       console.log("Connected to database!");
     })
     .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
+      console.error("Some error occurred while connecting to the database:", err);
     });
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
+// Check if the necessary environment variables are set
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, PORT } = process.env;
+
+if (!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET) {
+  throw new Error("Missing Cloudinary configuration environment variables.");
+}
+
+if (!PORT) {
+  throw new Error("Missing PORT environment variable.");
+}
+
+// Configure Cloudinary
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
   message: 'CLOUDINARY_SUCCESSFUL',
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
 });
